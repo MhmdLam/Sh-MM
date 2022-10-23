f5f1ac23dc7e94ec77d196eedb5c12b3a5f76a35
MhmdLama <mohammadbagherzadeh1380@gmail.com>
2022-10-23 22:00:02 +0330

    Add Cinemachine But Has a Bug in Zomout and Zomin

--- Assets/Scripts/VirtualCamera.cs
+++ Assets/Scripts/VirtualCamera.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class VirtualCamera : MonoBehaviour
 {
-    
+
     public Cinemachine.CinemachineVirtualCamera virtualcamera;
+    private bool CheckCameraZom = true;
+    public InputManager input;
     void Start()
     {
-        
+
     }
     void Update()
     {
         virtualcamera.Follow = PlayerController.player.transform;
-        virtualcamera.LookAt = PlayerController.player.transform;
+        //virtualcamera.LookAt = PlayerController.player.transform;
+        if (input.bodyChangeActive && CheckCameraZom)
+        {
+            CheckCameraZom = false;
+            ZomOutCamera();
+            Debug.Log("ZomOut");
+        }
+        else if (!input.bodyChangeActive && !CheckCameraZom)
+        {
+            CheckCameraZom = true;
+            CameraBackTpNormal();
+            Debug.Log("BackToNormal");
+        }
+    }
+    private void ZomOutCamera()
+    {
+        Cinemachine.CinemachineFramingTransposer framingTransposer = virtualcamera.AddCinemachineComponent<Cinemachine.CinemachineFramingTransposer>();
+        framingTransposer.m_CameraDistance=15f;
+
+    }
+    private void CameraBackTpNormal()
+    {
+        Cinemachine.CinemachineFramingTransposer framingTransposer = virtualcamera.AddCinemachineComponent<Cinemachine.CinemachineFramingTransposer>();
+        framingTransposer.m_CameraDistance=7.5f;
     }
 }
